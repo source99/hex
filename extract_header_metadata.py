eebbf991f0d7ea74a18635af8c9374ffde857922
Matt Rosenthal <matt.rosenthal@gmail.com>
2014-07-22 13:24:54 -0700

    files for making video csv

--- extract_header_metadata.py
+++ extract_header_metadata.py
@@ -14,7 +14,6 @@ meta_fp.write("Folder,USMH,DSMH,start time,stop time,start payout,stop payout,to
 
 
 
-
 #	def get_time(self,percent):
 
 
@@ -22,9 +21,11 @@ meta_fp.write("Folder,USMH,DSMH,start time,stop time,start payout,stop payout,to
 directory = "D:\\Dropbox (Centosette)_Hydromax\\PJ\\PuppytheaterUpload\\1539-Chattanooga,TN\\Source\\S138F004 S138J003_001"
 top_directory =  "D:\\Dropbox (Centosette)_Hydromax\\PJ\\PuppytheaterUpload\\1539-Chattanooga,TN\\Source\\"
 directories = glob.glob("{}\\*".format(top_directory))
+run_prefix = "E:/hmax/runs"
 print directories
 for directory in directories[2:]:
 	folder = directory.split("\\")[-1]
+    run_directory = "{}/{}".format(run_prefix,folder)
 
 	counter = Counter.Counter(directory)
 	hd_index = HD_index.HD_index(directory)
@@ -56,6 +57,17 @@ for directory in directories[2:]:
 			counter.get_closest_payout(hd_index.convert_scroll_to_time(curr_header.scrollStop)) - counter.get_closest_payout(hd_index.convert_scroll_to_time(curr_header.scrollStart)),
 			curr_header.endDistance
 			))
-
+        create_csv(directory, run_directory, header_file) 
 
 #print "closest to {} is {}".format(606688,counter.get_closest_payout(606688))
+
+
+
+#def create_csv(raw_directory, run_directory, header_file):
+#    localdata_short = os.path.split(run_directory)
+#
+#    hd_index = HD_index.HD_index(source_directory)
+#    sonar_counter = Counter.Counter(source_directory)
+#    sonar_data = Sonar_data.Sonar_data(run_directory)
+#    curr_header = Header.Header(header_file)
+
--- make_video_csv.py
+++ make_video_csv.py
@@ -2,47 +2,52 @@ import HD_index
 import Counter
 import Header
 import Sonar_data
+import os
 
-source_directory = "/Users/matt/work/centosette/hmax/Dropbox/PJ/PuppytheaterUpload/1539-Chattanooga,TN/Source/S138F004 S138J003_001"
-run_directory = "/Users/matt/work/centosette/hmax/runs/localdata_S138F004_S138J003_001"
-header_file = source_directory + "/Header_File_001.txt"
-localdata_short = "localdata_S138F004_S138J003_001"
+#source_directory = "/Users/matt/work/centosette/hmax/Dropbox/PJ/PuppytheaterUpload/1539-Chattanooga,TN/Source/S138F004 S138J003_001"
+#run_directory = "/Users/matt/work/centosette/hmax/runs/localdata_S138F004_S138J003_001"
+#header_file = source_directory + "/Header_File_001.txt"
+#localdata_short = "localdata_S138F004_S138J003_001"
 
 #HD frame, sonar frame, payout, water height, sediment height, sediment sq inches, date, time
 
+def create_csv(raw_directory, run_directory, header_file):
+    localdata_short = os.path.split(run_directory)
 
-hd_index = HD_index.HD_index(source_directory)
-sonar_counter = Counter.Counter(source_directory)
-sonar_data = Sonar_data.Sonar_data(run_directory)
-curr_header = Header.Header(header_file)
+    hd_index = HD_index.HD_index(source_directory)
+    sonar_counter = Counter.Counter(source_directory)
+    sonar_data = Sonar_data.Sonar_data(run_directory)
+    curr_header = Header.Header(header_file)
 
-start_time = hd_index.convert_scroll_to_time(curr_header.scrollStart)
-stop_time = hd_index.convert_scroll_to_time(curr_header.scrollStop) 
 
-hd_list_times = hd_index.get_range(start_time, stop_time)
 
-start_payout = sonar_counter.get_closest_payout(start_time)
-stop_payout = sonar_counter.get_closest_payout(stop_time) 
+    start_time = hd_index.convert_scroll_to_time(curr_header.scrollStart)
+    stop_time = hd_index.convert_scroll_to_time(curr_header.scrollStop) 
 
-csv_video_filename = "video_inputs.csv"
-csv_video = open(csv_video_filename,'w')
+    hd_list_times = hd_index.get_range(start_time, stop_time)
 
-#get date
-csv_video.write("hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage\n")
+    start_payout = sonar_counter.get_closest_payout(start_time)
+    stop_payout = sonar_counter.get_closest_payout(stop_time) 
 
-for frame_time in hd_list_times:
-    #get HD image name
-    hd_image_name = localdata_short + "/hires/jpg/" + hd_index.get_frame_name(frame_time)
-    sonar_index = sonar_data.get_frame_index_ms(frame_time)
-    sonar_image_name = localdata_short + "/masked/" + sonar_data.image_file_name[sonar_index]
-    clock_time_hour = hd_image_name[-15:-13]
-    clock_time_min = hd_image_name[-12:-10]
-    clock_time_ms = hd_image_name[-9:-4]
-    payout = sonar_counter.get_closest_payout(frame_time) 
-    water_height_inches = sonar_data.water_height_inches[sonar_index] 
-    sediment_height_inches = sonar_data.max_sed_height_inches[sonar_index]
-    capacity_missing_percentage = sonar_data.capacity_missing_percentage[sonar_index]
-    print "{},{},{},{},{},{},{},{},{},{}".format(hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage)
-    csv_video.write("{},{},{},{},{},{},{},{},{},{}\n".format(hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage))
+    csv_video_filename = "{}/video_inputs.csv".format(run_directory)
+    csv_video = open(csv_video_filename,'w')
 
-csv_video.close()
+    #get date
+    csv_video.write("hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage\n")
+
+    for frame_time in hd_list_times:
+        #get HD image name
+        hd_image_name = localdata_short + "/hires/jpg/" + hd_index.get_frame_name(frame_time)
+        sonar_index = sonar_data.get_frame_index_ms(frame_time)
+        sonar_image_name = localdata_short + "/masked/" + sonar_data.image_file_name[sonar_index]
+        clock_time_hour = hd_image_name[-15:-13]
+        clock_time_min = hd_image_name[-12:-10]
+        clock_time_ms = hd_image_name[-9:-4]
+        payout = sonar_counter.get_closest_payout(frame_time) 
+        water_height_inches = sonar_data.water_height_inches[sonar_index] 
+        sediment_height_inches = sonar_data.max_sed_height_inches[sonar_index]
+        capacity_missing_percentage = sonar_data.capacity_missing_percentage[sonar_index]
+        print "{},{},{},{},{},{},{},{},{},{}".format(hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage)
+        csv_video.write("{},{},{},{},{},{},{},{},{},{}\n".format(hd_image_name, sonar_image_name, frame_time, clock_time_hour, clock_time_min, clock_time_ms, payout, water_height_inches, sediment_height_inches, capacity_missing_percentage))
+
+    csv_video.close()
