d41bf2a1f349cfa721fa3b2c9a8f922f09008637
Matt Rosenthal <matt.rosenthal@gmail.com>
2014-08-01 13:48:05 -0700

    header update

--- Header.py
+++ Header.py
@@ -10,6 +10,9 @@ class Header:
         self.USMH = ""
         self.DSMH = ""
         self.radius = 0
+        self.pipe_size = 0
+        self.laser_start_frame = 0
+        self.laser_stop_frame = 0
         fp = open(file_name,'r')
         for line in fp:
             parts = line.split("=")
@@ -25,9 +28,14 @@ class Header:
                 self.day = int(parts[1])
             if "Pipe Size" in parts[0]:
                 self.radius = int(parts[1]) / 2
+                self.pipe_size = int(parts[1].rstrip())
             if "Downstream MH" in parts[0]:
                 self.DSMH = parts[1].rstrip()
             if "Upstream MH" in parts[0]:
                 self.USMH = parts[1].rstrip()
             if "End Distance" in parts[0]:
                 self.endDistance = float(parts[1].rstrip())
+            if "LaserOffsetStart" in parts[0]:
+                self.laser_start_frame = int(parts[1].rstrip())
+            if "LaserOffsetEnd" in parts[0]:
+                self.laser_stop_frame = int(parts[1].rstrip())
